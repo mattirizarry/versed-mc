ce56a9b3fdf3ee3f4f7a12191d88bda2aa7beddc
mattirizarry <matthewjirizarry@gmail.com>
2020-09-16 01:25:02 -0500

    Updated shop page

---
+++ src/pages/donate.tsx
@@ -0,0 +1,20 @@
+/** @jsx createElement */
+
+import { createElement } from 'react'
+import SEO from '@/components/SEO'
+import Layout from '@/components/Layout'
+
+const Shop = () => {
+  return (
+    <Layout>
+      <SEO 
+        title="Donate"
+      />
+      <section className="rank-comparision">
+
+      </section>
+    </Layout>
+  )
+}
+
+export default Shop
